07c9c744ffa4e62ca6a16dd789091b51086f936e
KimHyungIlko <hi465465@naver.com>
2020-07-20 11:30:09 +0900

    new files

--- src/screens/Emp/Analyze_GraphScreen.js
+++ src/screens/Emp/Analyze_GraphScreen.js
@@ -9,55 +9,60 @@ import {
   StackedBarChart,
 } from 'react-native-chart-kit';
 
-const screenWidth = Dimensions.get('window').width;
-
 class Analyze_GraphScreen extends Component {
   render() {
+    //graph data
+    const datas = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'], //x축 데이타셋
+      datasets: [
+        //y축 데이타셋
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+          ],
+        },
+      ],
+    };
+    //graph 모양 결정
+    const chartConfig = {
+      backgroundColor: '#e26a00',
+      backgroundGradientFrom: 'gray',
+      backgroundGradientTo: 'white',
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, //선색
+      labelColor: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, //라벨색
+      style: {
+        borderRadius: 16,
+      },
+      propsForDots: {
+        r: '6', //포인트 반지름
+        strokeWidth: '2', //포인트 태두리크기
+        stroke: '#ffa726', //포인트 태두리색
+      },
+    };
+    //전체 그래프의 radius값과 마진값 결정
+    const graphstyle = {
+      marginVertical: 8,
+      borderRadius: 16,
+    };
     return (
-      <View>
-        <Text>Bezier Line Chart</Text>
+      <View style={styles.center}>
+        <Text style={styles.head}>나의 야식대 사용 현황</Text>
         <LineChart
-          data={{
-            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-            datasets: [
-              {
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                ],
-              },
-            ],
-          }}
+          data={datas}
           width={Dimensions.get('window').width} // from react-native
           height={220}
           yAxisLabel="$"
           yAxisSuffix="k"
           yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            backgroundGradientFrom: 'gray',
-            backgroundGradientTo: 'white',
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, //선색
-            labelColor: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, //라벨색
-            style: {
-              borderRadius: 16,
-            },
-            propsForDots: {
-              r: '6', //포인트 반지름
-              strokeWidth: '2', //포인트 태두리크기
-              stroke: '#ffa726', //포인트 태두리색
-            },
-          }}
+          chartConfig={chartConfig}
           line
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
+          style={graphstyle}
         />
       </View>
     );
@@ -70,6 +75,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
+  },
+  head: {
+    color: 'red',
+    fontSize: 25,
+    fontWeight: 'bold',
   },
 });
 
