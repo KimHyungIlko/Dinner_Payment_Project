0b334a679a47d7ffc15ba9af55254fa3800a3196
KimHyungIlko <hi465465@naver.com>
2020-07-20 11:03:45 +0900

    new files

--- src/screens/Emp/Analyze_GraphScreen.js
+++ src/screens/Emp/Analyze_GraphScreen.js
@@ -1,11 +1,64 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+
+const screenWidth = Dimensions.get('window').width;
 
 class Analyze_GraphScreen extends Component {
   render() {
     return (
-      <View style={styles.center}>
-        <Text>Analyze_GraphScreen</Text>
+      <View>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: 'gray',
+            backgroundGradientTo: 'white',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, //선색
+            labelColor: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, //라벨색
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6', //포인트 반지름
+              strokeWidth: '2', //포인트 태두리크기
+              stroke: '#ffa726', //포인트 태두리색
+            },
+          }}
+          line
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
       </View>
     );
   }
