1eadf89deb8607fb5f72b00d0a2044ab7a088b79
KimHyungIlko <hi465465@naver.com>
2020-07-24 23:07:17 +0900

    fridya last

--- src/screens/Emp/Ret_ListScreen.js
+++ src/screens/Emp/Ret_ListScreen.js
@@ -15,22 +15,21 @@ import routes from '../../../routes';
 import axios from 'react-native-axios';
 // 식당 리스트
 const RetCard = ({retInfo, navigation}) => {
-  console.log ('retcard: ', retInfo.ret_img);
+  console.log('retcard: ', retInfo.ret_img);
   return (
     <TouchableOpacity
       style={styles.button}
       activeOpacity={0.5}
       onPress={() =>
-        navigation.navigate (routes.Req_Pay, {
+        navigation.navigate(routes.Req_Pay, {
           image: retInfo.ret_img,
           name: retInfo.name,
-        })}
-    >
+        })
+      }>
       <ImageBackground
         style={styles.img_back}
         imageStyle={{borderRadius: 10, borderColor: 'gray', borderWidth: 0.5}}
-        source={{uri: retInfo.ret_img}}
-      >
+        source={{uri: retInfo.ret_img}}>
         <Text style={styles.text}>{retInfo.name}</Text>
         <Text style={styles.text2}>매출: {retInfo.profit}원</Text>
       </ImageBackground>
@@ -39,22 +38,22 @@ const RetCard = ({retInfo, navigation}) => {
 };
 
 class Ret_ListScreen extends Component {
-  constructor (props) {
-    super (props);
+  constructor(props) {
+    super(props);
     this.state = {datas: []};
   }
 
-  async componentDidMount () {
-    let datas = await axios.get ('http://54.180.86.174/restaurants');
-    this.setState ({datas: datas.data});
+  async componentDidMount() {
+    let datas = await axios.get('http://54.180.86.174/restaurants');
+    this.setState({datas: datas.data});
   }
-  render () {
+  render() {
     const {datas} = this.state;
     const {navigation} = this.props;
     return (
       <View style={{backgroundColor: 'white'}}>
         <ScrollView>
-          {datas.map (data => {
+          {datas.map((data) => {
             return (
               <RetCard key={data.id} retInfo={data} navigation={navigation} />
             );
@@ -65,8 +64,8 @@ class Ret_ListScreen extends Component {
   }
 }
 
-const width = Dimensions.get ('screen').width;
-const styles = StyleSheet.create ({
+const width = Dimensions.get('screen').width;
+const styles = StyleSheet.create({
   button: {
     marginTop: 15,
     marginBottom: 5,
