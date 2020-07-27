10538c1dfc83bce846b49dd1989eaae43ae2e244
HYGill <slslalal12@gmail.com>
2020-07-27 16:43:17 +0900

    add Home

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, ImageBackground, Text, View, Image} from 'react-native';
 
 class HomeScreen extends Component {
   render() {
     return (
-      <View style={styles.center}>
-        <Text>This home screen</Text>
+      <View style={styles.container}>
+        <Image
+          source={require('../../../image/logo.png')}
+          style={styles.image}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#F3D599',
+  },
+  image: {
+    flex: 1,
+    width: 400,
+    height: 440,
     justifyContent: 'center',
     alignItems: 'center',
-    textAlign: 'center',
   },
 });
 
